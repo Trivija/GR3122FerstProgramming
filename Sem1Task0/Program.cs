728310b1b862c86e3b43f4a7009b941beb14a05d
Anna_Ponomareva <trivi-ja@mail.ru>
2022-09-08 16:06:07 +0300

    Добавили папки с урока 0,1,3,5,7

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//===========================================================================================================
+// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+//Например: 4 -> 16 -3 -> 9 -7 -> 49//
+//===========================================================================================================
+
+string? inputLine = Console.ReadLine(); //ввести значение //
+if (inputLine != null)   //!= не равно//
+{
+    int inputNamber = int.Parse(inputLine);  // int inputNamber выбор переменной////int.Parse(inputLine - преврати строкус в число //
+    int outNumber = 0;
+
+    DateTime d1 = DateTime.Now;
+    for (int i = 0; i < 10000000; i++)
+    {
+       outNumber = inputNamber * inputNamber;     //inputNamber*inputNamber -  число умноженное на само себя//
+    }
+
+DateTime d2 = DateTime.Now;
+Console.WriteLine(d2 - d1);
+
+Console.WriteLine(outNumber);      // вывести пользователю результат//
+}
+
+// ctrl+S - сохранить,   alt + shift + S - выровнить//
---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача No7
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе
+//показывает последнюю цифру этого числа.
+
+
+string? inputLine = Console.ReadLine();              //ввести значение //
+
+if (inputLine != null)                               //!= не равно//
+{
+    int inputNumber = int.Parse(inputLine);          // int inputNamber выбор переменной////int.Parse(inputLine - преврати строкус в число //
+    if (inputNumber > 99 && inputNumber < 1000)
+   {
+    int lastDigit = inputNumber % 10;
+    Console.Write(lastDigit);
+   }         
+}
+
+
+
+// ctrl+S - сохранить,   alt + shift + S - выровнить//
