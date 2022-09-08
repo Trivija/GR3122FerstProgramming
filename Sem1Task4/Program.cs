90853e5a64d37625acb0be1a3d5d4bca55bda918
Anna_Ponomareva <trivi-ja@mail.ru>
2022-09-08 20:53:12 +0300

    Добавили домашнее задание 2,4,6,8

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7    ;  44 5 78 -> 78
+
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.Write("максимальное число = ");
+// Console.WriteLine(max)
+
+
+int num1 = 44, num2 = 5, num3 = 78;
+ int max;
+ if (num1 >= num2)
+ {
+    if (num3 >= num1)
+    {
+       max = num3;
+    }
+    else
+    {
+       max = num1;
+    }
+ }
+ else
+ {
+    if (num3 >= num2)
+    {
+       max = num3;
+    }
+    else
+    {
+       max = num2;
+    }
+ }
+ Console.Write("максимальное число = ");
+Console.WriteLine(max);
---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,50 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нетusing System;
+ 
+
+ 
+
+namespace check1
+ 
+{
+ 
+    class Program
+ 
+    {
+ 
+        static void Main(string[] args)
+ 
+        {
+ 
+            int i;
+ 
+            Console.Write("Введите число : ");
+ 
+            i = int.Parse(Console.ReadLine());
+ 
+            if (i % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Введенное число является четным");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("Введенное число нечетное");
+ 
+                Console.Read();
+ 
+            }
+ 
+        }
+ 
+    }
+ 
+}
