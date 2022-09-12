15e56e043f32d7bd9bdeb0fe5082be0a511a481f
Anna_Ponomareva <trivi-ja@mail.ru>
2022-09-12 15:38:43 +0300

    Добавили изменения

---
+++ Sem1Task8_dz/Program.cs
@@ -0,0 +1,25 @@
+﻿// ===================================================================================================
+ //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+ //===============================================================================================
+ Console.WriteLine("Введите числo : ");
+
+ string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber < 2)
+    {
+        Console.WriteLine("Введите число больше 1");
+    }
+    int startIndex = 2;
+    while (startIndex <= inputNumber - 2)
+    {
+        Console.Write(startIndex + ", ");
+        startIndex += 2;
+    }
+    if (startIndex % 2 == 0)
+    {
+        Console.Write(startIndex);
+    }
+}
