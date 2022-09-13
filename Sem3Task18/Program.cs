5581106d6d88b3b49235e5b65c99b60cc7ed3f7a
Anna_Ponomareva <trivi-ja@mail.ru>
2022-09-13 17:38:24 +0300

    Добавили урок 18

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,34 @@
+﻿//==================================================================================================
+// Задача No18
+// Напишите программу, которая по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+//==================================================================================================
+
+int ReadData(string line)
+{
+    // выводим сообщение
+    Console.WriteLine(line);
+    //считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // возвращаем значение
+    return number;
+}
+string QoterBorderAsk(int numQoter)
+{
+    if (numQoter == 1)
+        return "x>0 y>0";
+    if (numQoter == 2)
+        return "x<0 y>0";
+    if (numQoter == 3)
+        return "x<0 y<0";
+    if (numQoter == 4)
+        return "x>0 y<0";
+    return "";
+}
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+int num = ReadData("Введите номер четверти: ");
+string res = QoterBorderAsk(num);
+PrintResult(res);
